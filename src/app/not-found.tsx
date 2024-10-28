5ad80af5285c876f2c24adba1cc2eae16fb685f9
GustavoAmorim12 <guamorim01@gmail.com>
2024-10-27 23:47:35 -0300

    estilizando o h1

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,6 +1,7 @@
 export default function NotFound() {
     return (
         <div className="bg-black text-white min-h-screen flex flex-col items-center justify-center">
+            <h1 className="text-4xl font-bold">404 - Not Found...</h1>
             
         </div>
     )
