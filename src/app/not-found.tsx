a59649daa3878fb6576a594093a2de4fe131cb17
GustavoAmorim12 <guamorim01@gmail.com>
2024-10-27 23:48:49 -0300

    arrumando estilizacao da div

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,8 +1,7 @@
 export default function NotFound() {
     return (
         <div className="bg-black text-white min-h-screen flex flex-col items-center justify-center">
-            <h1 className="text-4xl font-bold">404 - Not Found...</h1>
-            <p className="mt-4 text-lg">A página que você está procurando não existe.</p>
+            
         </div>
     )
 }
