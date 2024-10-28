30a162bc5f2e8fb82842bd8add7e600049ed61e6
GustavoAmorim12 <guamorim01@gmail.com>
2024-10-27 23:44:44 -0300

    criando o classname

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,6 +1,7 @@
 export default function NotFound() {
     return (
-        <div></div>
-        
+        <div className="bg-black text-white min-h-screen flex flex-col items-center justify-center">
+
+        </div>
     )
 }
